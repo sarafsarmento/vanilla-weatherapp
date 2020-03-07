1e659f846eae795dfb4761132f11fbaf90179410
sarafsarmento <sarafsarmento@gmail.com>
2020-03-07 18:55:09 +0000

    Update script.js

--- src/script.js
+++ src/script.js
@@ -37,6 +37,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "d7642530016d8d9da61efd1db83c3388";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "New York";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
