8178e6c3a9f13ab681d86f10f7cda4cdafbcd448
sarafsarmento <sarafsarmento@gmail.com>
2020-03-08 16:45:40 +0000

    Added search form

--- src/script.js
+++ src/script.js
@@ -42,8 +42,19 @@ function displayTemperature(response) {
   weatherIconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "d7642530016d8d9da61efd1db83c3388";
-let city = "Lisbon";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+  let apiKey = "d7642530016d8d9da61efd1db83c3388";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+addEventListener("submit", handleSubmit);
